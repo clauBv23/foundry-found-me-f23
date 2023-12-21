56a4ee48ba9acad6331c25e63c1118239aa071d0
Claudia <claudiabarcelovaldes40@gmail.com>
2023-12-21 23:01:37 +0100

    add tests

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -2,17 +2,28 @@
 
 pragma solidity ^0.8.19;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {FundMeDeployment} from "../script/FundMeDeployment.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+    FundMeDeployment deployment;
 
     function setUp() external {
-        fundMe = new FundMe(address(0));
+        deployment = new FundMeDeployment();
+        fundMe = deployment.run();
     }
 
     function testMinimumDolarIsFive() public {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsSender() public {
+        assertEq(fundMe.getOwner(), msg.sender);
+    }
+
+    function testPriceFeedVersionIsAccurate() public {
+        assertEq(fundMe.getVersion(), 4);
     }
 }
